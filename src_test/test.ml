ae0f07ea4bf4e9481bff2767ff250a1f15f1c2e7
Roma Sokolov <sokolov.r.v@gmail.com>
2016-03-15 15:59:46 +0300

    [minor] tests

--- src_test/test.ml
+++ src_test/test.ml
@@ -58,10 +58,21 @@ module Nullable = struct
 
 type outside = Something of Nullable.user [@@deriving jsobject]
 
-let json_parse json =
-  let jclass = (Js.Unsafe.variable "JSON") in
-  let jss = Js.string json in
-  Js.Unsafe.meth_call jclass "parse" [| Js.Unsafe.inject jss |]
+module M = struct
+  type t = int [@@deriving jsobject]
+end
+type out = Other of M.t [@@deriving jsobject]
+
+module JSON = struct
+  let json = (Js.Unsafe.variable "JSON")
+
+  let parse j =
+    let jss = Js.string j in
+    Js.Unsafe.meth_call json "parse" [| Js.Unsafe.inject jss |]
+
+  let stringify obj =
+    Js.Unsafe.meth_call json "stringify" [| obj |]
+end
 
 
 let show_user = function
@@ -72,51 +83,31 @@ let show_user = function
 (* fix this *)
 module Pjcr = Ppx_jsobject_conv_runtime
 
+let run_test name inp conv_func show_func =
+  (* add "expected" flag or use ounit *)
+  let parsed = JSON.parse inp in
+  match conv_func parsed with
+  | Ok(converted) ->
+     Printf.printf "OK: [%s] %s  -- %s\n" name inp (show_func converted)
+  | Error(msg) ->
+     Printf.printf "ERR: [%s] %s -- %s\n" name inp msg
+
 let ()=
   let open Result in
   let full_user = "{\"age\": 18, \"name\":\"Varya\", \"status\":[\"Created\"]}" in
   let partial_user1 = "{}" in
   let partial_user2 = "{\"age\": 24}" in
   let partial_user3 = "{\"age\": 12, \"name\":\"vasya\"}" in
-  let fuo = json_parse full_user in
-  let puo1 = json_parse partial_user1 in
-  let puo2 = json_parse partial_user2 in
-  let puo3 = json_parse partial_user3 in
-  let () = Firebug.console##log (fuo) in
-  let goss = JSON.parse go_style_struct in
-  let fu_res = user_of_jsobject_res fuo in
-  let should_fail_fu_res = user_of_jsobject_res puo1 in
-  let nu0_res = Nullable.user_of_jsobject_res fuo in
-  let nu1_res = Nullable.user_of_jsobject_res puo1 in
-  let nu2_res = Nullable.user_of_jsobject_res puo2 in
-  let nu3_res = Nullable.user_of_jsobject_res puo3 in
-  let goss_res = go_style_struct_of_jsobject_res goss in
-  let () = match fu_res with
-    | Ok(fu) -> Printf.printf "fuo OK: %s\n" (show_user fu)
-    | Error(s) -> Printf.printf "fuo ERR: %s\n" (s)
-  in
-  let () = match should_fail_fu_res with
-    | Ok(fu) -> Printf.printf "should_fail OK: %s\n" (show_user fu)
-    | Error(s) -> Printf.printf "should_fail ERR: %s\n" (s)
-  in
-  let () = match nu0_res with
-    | Ok(nu) -> Printf.printf "nu0 OK: %s\n" (Nullable.show_user nu)
-    | Error(s) -> Printf.printf "nu0 ERR: %s\n" (s)
-  in
-  let () = match nu1_res with
-    | Ok(nu) -> Printf.printf "nu1 OK: %s\n" (Nullable.show_user nu)
-    | Error(s) -> Printf.printf "nu1 ERR: %s\n" (s)
-  in
-  let () = match nu2_res with
-    | Ok(nu) -> Printf.printf "nu2 OK: %s\n" (Nullable.show_user nu)
-    | Error(s) -> Printf.printf "nu2 ERR: %s\n" (s)
-  in
-  let () = match nu3_res with
-    | Ok(nu) -> Printf.printf "nu3 OK: %s\n" (Nullable.show_user nu)
-    | Error(s) -> Printf.printf "nu3 ERR: %s\n" (s)
-  in
-  let () = match goss_res with
-    | Ok(g) -> Printf.printf "goss OK: %s\n" (show_go_style_struct g)
-    | Error(s) -> Printf.printf "goss ERR: %s\n" s
-  in
-  ()
+  let faulty_full_user = "{\"age\": 18, \"status\":[\"Created\"]}" in
+  let go_style_struct = "{\"FieldName\": \"some field name\"}" in
+  run_test "full_user" full_user user_of_jsobject_res show_user;
+  run_test "partial1" partial_user1
+           Nullable.user_of_jsobject_res Nullable.show_user;
+  run_test "partial2" partial_user2
+           Nullable.user_of_jsobject_res Nullable.show_user;
+  run_test "partial3" partial_user3
+           Nullable.user_of_jsobject_res Nullable.show_user;
+  run_test "faulty" faulty_full_user
+           user_of_jsobject_res show_user;
+  run_test "go_style" go_style_struct
+           go_style_struct_of_jsobject_res show_go_style_struct;
