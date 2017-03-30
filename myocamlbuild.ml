e1dd952686c118b199d8261c169da6ecd76b492c
Roma Sokolov <sokolov.r.v@gmail.com>
2017-03-30 21:56:56 +0300

    [minor] cleanup

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -5,10 +5,6 @@ let () = dispatch (
   | After_rules ->
      flag ["ocaml"; "compile"; "use_ppx_jsobject_conv"] &
        S[A"-ppx"; A("./bin/ppx_jsobject.byte -as-ppx")];
-     (* flag ["ocaml"; "link"; "use_ppx_jsobject_conv"; "byte"] & *)
-     (*   A("./src/ppx_jsobject_conv_runtime.cma"); *)
-     (* flag ["ocaml"; "link"; "use_ppx_jsobject_conv"; "native"] & *)
-     (*   A("./src/ppx_jsobject_conv_runtime.cmxa"); *)
      ocaml_lib ~dir:"src" "src/ppx_jsobject_conv_runtime";
      (* Pass -predicates to ocamldep *)
      pflag ["ocaml"; "ocamldep"] "predicate" (fun s -> S [A "-predicates"; A s])
